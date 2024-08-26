86ef7b0a534de8fa174b650d67493097c49abc8d
soumyadeepmajumder04@gmail.com <soumyadeepmajumder04@gmail.com>
2024-08-26 20:05:54 +0000

    Edited Home

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -11,7 +11,7 @@ from ..Switchtosportsmode import Switchtosportsmode
 
 
 class Home(HomeTemplate):
-   def __init__(self, **properties):
+  def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
