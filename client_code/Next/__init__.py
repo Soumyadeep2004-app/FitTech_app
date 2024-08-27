ca42e06fa951de8cc580b2cd8abf76481195e2a9
soumyadeepmajumder04@gmail.com <soumyadeepmajumder04@gmail.com>
2024-08-27 23:03:42 +0000

    Created Screenshot_28-8-2024_42010_.jpeg, Next and Screenshot_28-8-2024_35250_.jpeg; Edited Healthyhabits

--- client_code/Next/__init__.py
+++ client_code/Next/__init__.py
@@ -1,23 +1,15 @@
-from ._anvil_designer import HealthyhabitsTemplate
+from ._anvil_designer import NextTemplate
 from anvil import *
 import anvil.server
 import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-from ..Next import Next
 
 
-class Healthyhabits(HealthyhabitsTemplate):
+class Next(NextTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-
-
-  def next_link_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    self.content_panel.clear()
-    self.content_panel.add_component(Next())
-    
