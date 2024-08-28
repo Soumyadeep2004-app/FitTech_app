62c808353b03a843fbe0b5b79eadbc29a213f077
soumyadeepmajumder04@gmail.com <soumyadeepmajumder04@gmail.com>
2024-08-28 12:53:34 +0000

    Created Cricket, WhatsApp Image 2024-08-28 at 3.15.05 PM.jpeg, WhatsApp Image 2024-08-28 at 3.19.55 PM.jpeg, WhatsApp Image 2024-08-28 at 3.25.35 PM.jpeg and 7 more; Edited Aboutus, Back, Bulking, Chest and 19 more

--- client_code/Reports/__init__.py
+++ client_code/Reports/__init__.py
@@ -1,5 +1,8 @@
 from ._anvil_designer import ReportsTemplate
 from anvil import *
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.facebook.auth
 import plotly.graph_objects as go
 import anvil.server
 import anvil.users
